5873924ca9b7c4c5f9e503268135633081902fff
bichief <89649583+bichief@users.noreply.github.com>
2022-02-21 19:01:35 +0300

    Update send_for_admin.py

--- utils/send_for_admin.py
+++ utils/send_for_admin.py
@@ -3,12 +3,15 @@ from loader import bot
 array = [1955750981, 417436565]
 
 
-async def new_customer(name, phone):
+async def new_customer(name, phone, time, day, service):
     try:
         for row in array:
             await bot.send_message(chat_id=row,
                                    text=f'К вам записалась {name}\n'
                                         f'Контактный номер - {phone}\n\n'
+                                        f'Время записи - {time}\n'
+                                        f'День недели - {day}\n'
+                                        f'Название услуги - {service}'
                                         f'Созвонитесь, дабы узнать подробности :)')
     except:
         pass
