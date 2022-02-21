6318777845d50828e72ba766c42cf195e6346e1e
bichief <89649583+bichief@users.noreply.github.com>
2022-02-21 19:41:53 +0300

    Update time_markup.py

--- keyboards/inline/time_markup.py
+++ keyboards/inline/time_markup.py
@@ -3,16 +3,16 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils.db_api.commands.time_service import get_time
 
 
-async def time_markup(name, day):
+async def time_markup(day):
     global btn
     time = InlineKeyboardMarkup(row_width=2)
-    array = await get_time(name=name, day=day)
+    array = await get_time(day=day)
     if not array:
         return False
 
     for row in array:
         regex = row.split('&')
-        btn = InlineKeyboardButton(text=f'{regex[0]}', callback_data=f'time_{regex[0]}_{name}_{regex[1]}')
+        btn = InlineKeyboardButton(text=f'{regex[0]}', callback_data=f'time_{regex[0]}_{regex[1]}')
         time.add(btn)
 
     return time
